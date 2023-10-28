4b98bb3a6460465bd7093f1d284ef347e6e34993
Kerend_ Jo <kerendjonathan3@gmail.com>
2023-10-28 09:09:05 +0700

    SMKDEV Submission

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"net/http"
+	"strconv"
 
 	"github.com/labstack/echo/v4"
 )
@@ -34,11 +35,18 @@ func initRoutes() {
 // TODO Problem and Testing 1: Buat Fungsi yang memberikan seluruh informasi student
 func getStudents(c echo.Context) error {
 	// Tulis jawabanmu disini
+	return c.JSON(http.StatusOK, students)
 }
 
 // TODO Problem and Testing 2: Buat Fungsi yang memberikan informasi student berdasarkan ID
 func getStudent(c echo.Context) error {
 	// Tulis jawabanmu disini
+	id := c.Param("id")
+	for _, student := range students {
+		if strconv.Itoa(student.ID) == id {
+			return c.JSON(http.StatusOK, student)
+		}
+	}
 
 	return c.JSON(http.StatusOK, students)
 }
@@ -47,12 +55,29 @@ func getStudent(c echo.Context) error {
 func createStudent(c echo.Context) error {
 	// Tulis jawabanmu disini
 
+	student := new(Student)
+	if err := c.Bind(student); err != nil {
+		return err
+	}
+	students = append(students, *student)
 	return c.JSON(http.StatusCreated, student)
 }
 
 // TODO Problem and Testing 4: Buat Fungsi yang dapat mengupdate informasi student berdasarkan ID
 func updateStudent(c echo.Context) error {
 	// Tulis jawabanmu disini
+	id := c.Param("id")
+	updatedStudent := new(Student)
+	if err := c.Bind(updatedStudent); err != nil {
+		return err
+	}
+
+	for i, student := range students {
+		if strconv.Itoa(student.ID) == id {
+			students[i] = *updatedStudent
+			return c.JSON(http.StatusOK, students)
+		}
+	}
 
 	return c.JSON(http.StatusOK, students)
 }
@@ -60,6 +85,14 @@ func updateStudent(c echo.Context) error {
 // TODO Problem and Testing 5: Buat Fungsi yang dapat menghapus informasi student berdasarkan ID
 func deleteStudent(c echo.Context) error {
 	// Tulis jawabanmu disini
+	id := c.Param("id")
+
+	for i, student := range students {
+		if strconv.Itoa(student.ID) == id {
+			students = append(students[:i], students[i+1:]...)
+			return c.NoContent(http.StatusNoContent)
+		}
+	}
 
 	return c.JSON(http.StatusNoContent, students)
 }
